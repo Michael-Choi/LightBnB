9c5dda9b3dcb5a23c4ecf0ac8c04d27bd4b4d5d3
Michael-Choi <Michael.choi@queensu.ca>
2019-09-19 04:10:09 +0000

    added get all reservations function to database.js

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -77,7 +77,30 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+    .query(
+      `
+  SELECT reservations.*, properties.*, avg(rating) FROM users
+  JOIN reservations ON
+  users.id=guest_id
+  JOIN properties ON
+  properties.id=reservations.property_id
+  JOIN property_reviews ON
+  properties.id=property_reviews.property_id
+  WHERE end_date<now()::date and users.id=$1
+  GROUP BY properties.id, reservations.id
+  ORDER BY start_date
+  LIMIT $2;
+  `,
+      [guest_id, limit]
+    )
+    .then(res => res.rows);
+
+  // SELECT * FROM RESERVATIONS
+  // WHERE GUEST_ID=$1
+  // LIMIT $2;
+
+  //return getAllProperties(null, 2);
 };
 exports.getAllReservations = getAllReservations;
 
